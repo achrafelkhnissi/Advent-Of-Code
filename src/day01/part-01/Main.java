70d5e4ed609dd4ac461b9932092cc9f78126da53
Achraf EL Khnissi <achraf.elkhnissi@gmail.com>
2023-12-04 17:11:08 +0100

    add: part 01 - done

---
+++ src/day01/part-01/Main.java
@@ -0,0 +1,40 @@
+package day01;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+
+        String filePath = "src/day01/input.txt";
+        int sum = 0;
+
+        try {
+            List<String> lines = Files.readAllLines(Path.of(filePath));
+
+            for (String line : lines) {
+                List<Integer> numbers = line.chars()
+                        .filter(Character::isDigit)
+                        .map(Character::getNumericValue)
+                        .boxed()
+                        .toList();
+
+                if (numbers.isEmpty()) {
+                    continue;
+                }
+
+                int firstDigit = numbers.get(0);
+                int lastDigit = numbers.get(numbers.size() - 1);
+
+                sum += Integer.parseInt(firstDigit + "" + lastDigit);
+
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        System.out.println(sum);
+    }
+}
